6c63014f18ed2df0e7f5f392cb4d5ceb5fb333ff
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-08-29 11:19:51 +0530

    added sign in  , login , logout API and implemented user DB

---
+++ routes/companyRoutes.js
@@ -0,0 +1,49 @@
+import express from "express";
+import Company from "../models/Company.js"
+const companyRouter = express.Router();
+import { userAuth } from "../middlewares/auth.js";
+
+
+companyRouter.post("/api/companies", userAuth , async (req, res) => {
+    try {
+      const company = await Company.create(req.body);
+      console.log("✅ New company created");
+      return res.status(201).json(company);
+    } catch (e) {
+      console.error("❌ Error creating company:", e.message);
+      return res.status(400).json({ error: e.message });
+    }
+  });
+
+
+
+  companyRouter.get("/api/companies", userAuth ,async (req, res) => {
+    try {
+  
+      const companies = await Company.find({}, "name type eligibility roles count");
+      
+      return res.json(companies);
+    } catch (e) {
+      console.error("❌ Error fetching companies:", e.message);
+      return res.status(500).json({ error: "Server error" });
+    }
+  });
+
+
+
+
+  companyRouter.get("/api/companies/:id", userAuth , async (req, res) => {
+    try {
+      const company = await Company.findById(req.params.id);
+      if (!company) {
+        return res.status(404).json({ error: "Company not found" });
+      }
+      return res.json(company);
+    } catch (e) {
+      console.error("❌ Error fetching company:", e.message);
+      return res.status(400).json({ error: "Invalid company ID" });
+    }
+  });
+
+
+export default companyRouter;
