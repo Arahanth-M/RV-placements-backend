020e8b648a7a98415bf2f1194f2c38780ecf6257
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-09-03 16:56:21 +0530

    removed s3-client

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -3,22 +3,10 @@ import Company from "../models/Company.js"
 const companyRouter = express.Router();
 import { userAuth } from "../middlewares/auth.js";
 import dotenv from 'dotenv';
-import { S3Client,PutObjectCommand } from '@aws-sdk/client-s3';
 dotenv.config();
 
 
-const bucketName = process.env.BUCKET_NAME;
-const bucketRegion = process.env.BUCKET_REGION;
-const accessKey = process.env.ACCESS_KEY;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 
-const s3 = new S3Client({
-  credentials: {
-    accessKeyId: accessKey,
-    secretAccessKey: secretAccessKey,
-  },
-  region: bucketRegion,
-});
 
 
 companyRouter.post("/api/companies", userAuth , async (req, res) => {
